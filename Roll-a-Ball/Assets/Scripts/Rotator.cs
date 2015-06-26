61d7227d2c2a751d4ba5bc4d359a1693d805166b
Jonathan Rawling <jonathan@rawling.me>
2015-06-26 10:28:40 +0100

    Initial commit
    Completed "roll-a-ball" project and new "space shooter" project.

---
+++ Roll-a-Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour 
+{
+	private Vector3 rotation;
+
+	void Start()
+	{
+		rotation = new Vector3 (15, 30, 45);
+	}
+
+	void Update()
+	{
+		transform.Rotate (rotation * Time.deltaTime);
+	}
+}
