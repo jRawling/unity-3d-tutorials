eb83d975b1068004a122a40f917b03115536e73b
Jonathan Rawling <jonathan@rawling.me>
2015-06-27 20:28:42 +0100

    Space Shooter work
    Added bolts, hazards, explosions and lifecycle management.

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	public GameObject asteroidExplosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Boundary")  { return; }
+		Instantiate (asteroidExplosion, transform.position, transform.rotation);
+		if (other.tag == "Player") 
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy(other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+	public GameObject shot;
+	public float fireRate;
+	public Transform shotSpawn;
+	private Rigidbody rb;
+	private float nextFire;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+	
+		rb.velocity = new Vector3 (moveHorizontal, 0.0f, moveVertical) * speed;
+		rb.position = new Vector3 
+       (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax) 
+		);
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
